4e5fba5ff57a0c9a83ef3e891c4ea2f12b80b077
TomTom830 <thomasmartins83@gmail.com>
2019-06-02 16:08:02 +0200

    ajout condition champ pas remplie register

--- src/main/resources/static/js/registration.js
+++ src/main/resources/static/js/registration.js
@@ -3,19 +3,25 @@ angular.module('PolyNet_registration',[]).controller('mainController', function 
     $scope.User = {};
 
    $scope.inscription = function() {
-       $http.post("/register",$scope.User).then(function (value) {
-           console.log(value.data.response);
-           var message = document.getElementsByClassName("nom_deja_pris")[0];
-           message.style.visibility = "visible";
+       if ($scope.User.username == "" || typeof $scope.User.username == "undefined"
+       || $scope.User.password == "" || typeof $scope.User.password == "undefined") {
 
-           if(value.data.response != "OK") {
-               message.innerHTML = value.data.response;
-           }
-           else{
-               message.innerHTML = "Inscription reussie";
-               $scope.User = {};
-               window.location.replace("/");
-           }
-       });
+           alert("Veuillez remplir tout les champs");
+       }
+       else{
+           $http.post("/register", $scope.User).then(function (value) {
+               console.log(value.data.response);
+               var message = document.getElementsByClassName("nom_deja_pris")[0];
+               message.style.visibility = "visible";
+
+               if (value.data.response != "OK") {
+                   message.innerHTML = value.data.response;
+               } else {
+                   message.innerHTML = "Inscription reussie";
+                   $scope.User = {};
+                   window.location.replace("/");
+               }
+           });
+       }
    }
 });
